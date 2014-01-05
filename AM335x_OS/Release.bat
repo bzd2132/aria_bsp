48256a9a2192c186ac4de75618630e17df785858
tom.chen <chenzhetao@gmail.com>
2014-01-06 00:08:58 +0800

    [Aria]: Initial version of aria build

---
+++ AM335x_OS/Release.bat
@@ -0,0 +1,6 @@
+@echo "Copy binary to destination folder"
+
+copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\EBOOTSD.nb0 ..\..\WEC7\aria\
+copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\MLO ..\..\WEC7\aria\
+copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\XLDRSD.bin ..\..\WEC7\aria\
+copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\NK.bin ..\..\WEC7\aria\
