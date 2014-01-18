c128d1e1c594f2dd3b621f0511571eea796af00e
tom.chen <chenzhetao@gmail.com>
2014-01-18 16:22:03 +0800

    [Aria]:reduce image size and add logo.nb0

--- AM335x_OS/Release.bat
+++ AM335x_OS/Release.bat
@@ -5,4 +5,5 @@ copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\EBOOTSD.nb0 ..\..\
 copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\XLDRSD.nb0 ..\..\WEC7\aria\
 copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\EBOOTND.nb0 ..\..\WEC7\aria\
 copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\XLDRNAND.nb0 ..\..\WEC7\aria\
+copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\logo.nb0 ..\..\WEC7\aria\
 copy /Y .\AM335x_OS\RelDir\AM335X_BSP_AriaBoard_ARMV7_Release\NK.bin ..\..\WEC7\aria\
